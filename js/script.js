190b2bb5cb468e6015af8f9aa934ce54e4c92605
Jerocam86 <ocamposjero@gmail.com>
2020-02-06 23:29:30 -0800

    done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-// STEP 1
-// STEP 2
+// STEP 1 
+var someMonth;
+
+function theMonth();
+
+var currentMonth;
+
+var summerMonth;
+
+var myLibraryFunction;
+
+// STEP 2 
+let jerocam = 19;
+let lastMotherName = "Escobar";
+let believe = true;
+      if (believe) { 
+       }
+let x = null;
+
+
 // STEP 3
+
