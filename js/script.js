e705aa83e6027ac7f6e30569b90f4a5cda5e8168
Jeronimo Jose Ocampos <ocamposjero@gmail.com>
2020-02-16 11:21:13 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
 // STEP 1 
 var someMonth;
-
 function theMonth();
-
 var currentMonth;
-
 var summerMonth;
-
 var myLibraryFunction;
 
 // STEP 2 
@@ -17,6 +13,105 @@ let believe = true;
        }
 let x = null;
 
-
 // STEP 3
+Var month = “July”;
+var telePhone = 91865874; 
+
+//STEP 4
+let firstName = “Jeronimo”;
+let lastName = “Ocampos”;
+let address = “8300 Station Village 13”;
+let city = “San Diego”;
+let state = “California”;
+let zipCode = 92108;
+let yourAge = 33; 
+let referralSource = 321;
+let mayWeContactYou = “ocamposjero@gmail.com”;
+
+//STEP 5
+document.getElementById(“demo”).innerHtml = firstName, lastName, address, city, state, zipCode, yourAge, referralSource, mayWeContactYou;
+console.log(firstName, lastName, address, city, state, zipCode, yourAge, referralSource, mayWeContactYou);
+
+//STEP 6
+let stars = 8; 
+let horoscop = “Taurus”;
+window.console.log(“The number of the”+horoscop+ “ is”+stars+);
+
+//STEP 7
+Let firstResult = Boolean(“rugby”);    //true
+let secondResult = Boolean(150);    //true
+
+//STEP 8
+var someString = "Who once said, \"Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.\"";
+window.console.log(someString);
+
+//STEP 9
+window.console.log(jero=null);
+window.console.log(jero=undefined);
+
+//STEP 10
+window.console.log(typeof “Juan”);   //string
+window.console.log(typeof  6.15);    //number
+window.console.log(typeof  false);   //boolean
+window.console.log(typeof  new Date()); //object
+window.console.log(typeof  myAge);  //undefined
+
+//STEP 11
+alert(“Hello” +subsName ”, welcome to the JavaScript class!”);
+
+//STEP 12
+let helloName = “Zak”;
+let subsName = helloName.replace(“Zak”, “Jero”); 
+alert(subsName);
+
+//STEP 13
+let course = “JavaScript”;
+alert(course);
+
+//STEP 14
+alert(“Hello Zak Ruvalcaba.\nWelcome to the JavaScript class!”);
+
+//STEP 15
+let response = prompt (“Hello Jeronimo”);
+let theirName = response.replace(“Jeronimo”, +response);
+
+//STEP 16
+
+
+//STEP 17
+let x = 10;
+let y = 20;
+window.console.log(x+y);
+
+//STEP 18
+let x = 20;
+window.console.log(x +=x);
+
+//STEP 19
+let x = 20;
+window.console.log(x*5);
+
+//STEP 20
+let x = 20;
+window.console.log(x%3);
+
+//STEP 21
+let x = 6;
+window.console.log(x==6);
+
+//STEP 22
+let y = 21;
+window.console.log(x!=21);
+
+//STEP 23
+let widget = {game:“God of War”, type:”action”, year:2019};
+window.console.log(typeof widget.game);
+
+//STEP 24
+let widget = [“clock”, “keyword”, “calculator”];
+window.console.log(widget instanceof Array);
+
+//STEP 25
+let widget = [“clock”, “keyword”, “calculator”];
+window.console.log(widget instanceof Number);
 
